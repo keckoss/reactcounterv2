00c41f4e4c72146bb2eb3ea4557f2f2d75a8645f
keckoss13 <73256718+keckoss13@users.noreply.github.com>
2023-07-06 19:58:57 +0200

    la- misere

--- src/assets/components/Counter.jsx
+++ src/assets/components/Counter.jsx
@@ -1,33 +1,25 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 function Counter() {
-  const [counter, setCounter] = useState(0);
+  const [counters, setCounters] = useState([0]);
 
   const addCounter = () => {
-    setCounter(counter + 1);
+    setCounters([...counters, 0]);
   };
 
   return (
-    <>
-      <div>
-        <button
-          onClick={() => {
-            const newCounter = [...counter];
-            newCounter.push(counter);
-            setCounter(newCounter);
-          }}
-        >
-          ADD COUNTER
-        </button>
-
-        <div>
-          <button onClick={() => setCounter(counter + 1)}>PLUS</button>
+    <div>
+      <button onClick={addCounter}>Add Counter</button>
+      {counters.map((counter, index) => (
+        <div key={index}>
+          <button>+</button>
           <p>{counter}</p>
-          <button onClick={() => setCounter(counter - 1)}>MOINS</button>
-          <button onClick={() => setCounter(0)}>RESET</button>
+          <button>-</button>
+          <button>Reset</button>
         </div>
-      </div>
-    </>
+      ))}
+    </div>
   );
 }
+
 export default Counter;
